eccb6dbb30e433e062863d17ca69c021562fee35
YoungQWER <wkdrnr321@naver.com>
2024-06-05 12:49:39 +0900

    first commit

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Log4j2
@@ -35,4 +37,47 @@ public class ThymeleafExController {
         return "thymeleaf/thymeleafEx02";
     }
 
+    @GetMapping(value = "/ex03")
+    public String ex03(Model model) {
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=0; i<10; i++) {
+
+            ItemDto itemDto = ItemDto.builder()
+                    .itemNm("테스트 상품" + i)
+                    .itemDetail("상품 상세 설명" + i)
+                    .price(10000*i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+
+        return "thymeleaf/thymeleafEx03";
+    }
+
+    @GetMapping(value = "/ex04")
+    public String ex04(Model model) {
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=0; i<10; i++) {
+
+            ItemDto itemDto = ItemDto.builder()
+                    .itemNm("테스트 상품" + i)
+                    .itemDetail("상품 상세 설명" + i)
+                    .price(10000*i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+
+        return "thymeleaf/thymeleafEx04";
+    }
 }
