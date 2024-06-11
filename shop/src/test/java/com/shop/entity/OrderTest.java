2300391081025f165d7977c111e825a0b4c8da22
YoungQWER <wkdrnr321@naver.com>
2024-06-11 14:23:00 +0900

    first commit

--- shop/src/test/java/com/shop/entity/OrderTest.java
+++ shop/src/test/java/com/shop/entity/OrderTest.java
@@ -3,6 +3,7 @@ package com.shop.entity;
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepostory;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
@@ -35,6 +36,25 @@ class OrderTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTest(){
+
+        Order order = this.createOrder();
+
+        Long orderItemid = order.getOrderItems().get(0).getId();
+
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem = orderItemRepository.findById(orderItemid)
+                .orElseThrow(EntityNotFoundException::new);
+        log.info("orderItem : " + orderItem.getOrder().getClass());
+    }
 
     public Item createItem(){
         Item item = new Item();
