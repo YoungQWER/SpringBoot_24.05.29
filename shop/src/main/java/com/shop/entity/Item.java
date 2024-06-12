b5fa631f0d310402d133558fa458ee22842dbc41
YoungQWER <wkdrnr321@naver.com>
2024-06-12 10:40:26 +0900

    first commit

--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -17,6 +17,7 @@ public class Item extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
     private Long id;
 
     @Column(nullable = false, length = 50)
