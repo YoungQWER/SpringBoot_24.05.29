2762ee7b47a60771619386a22c74a56a7701a817
YoungQWER <wkdrnr321@naver.com>
2024-07-05 15:48:04 +0900

    first commit

--- todoExam/demo/src/main/java/com/example/demo/controller/UserController.java
+++ todoExam/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.model.UserEntity;
+import com.example.demo.security.TokenProvider;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final TokenProvider tokenProvider;
 
     // 회원가입
     @PostMapping("/signup")
@@ -45,6 +47,31 @@ public class UserController {
 
         }catch(Exception e){
             ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    // 로그인
+    @PostMapping("/signin")
+    public ResponseEntity<?> loginUser(@RequestBody UserDTO userDTO) {
+        UserEntity user = userService.getByCredentials(userDTO.getUsername(), userDTO.getPassword());
+
+        if(user != null){
+
+            String token = tokenProvider.create(user);
+
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .username(user.getUsername())
+                    .id(user.getId())
+                    .build();
+
+            return ResponseEntity.ok(responseUserDTO);
+
+        }else{
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login Error")
+                    .build();
+
             return ResponseEntity.badRequest().body(responseDTO);
         }
     }
